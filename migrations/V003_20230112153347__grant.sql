78a2b1fdbdd754b053ec1625feccec0ddf138b53
Grant Fritchey <grant@scarydba.com>
2023-01-12 09:52:23 -0600

    drop and recreate view as part of testing.

---
+++ migrations/V003_20230112153347__grant.sql
@@ -0,0 +1,26 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Creating [HumanResources].[GettingData]'
+GO
+CREATE VIEW [HumanResources].[GettingData]
+AS
+SELECT BusinessEntityID,
+       NationalIDNumber,
+       LoginID,
+       OrganizationNode,
+       OrganizationLevel,
+       JobTitle,
+       BirthDate,
+       MaritalStatus,
+       Gender,
+       HireDate,
+       SalariedFlag,
+       VacationHours,
+       SickLeaveHours,
+       CurrentFlag,
+       rowguid,
+       ModifiedDate FROM HumanResources.Employee;
+GO
+
