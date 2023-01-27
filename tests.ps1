ca2ba608aaf8d572f0e66e40dde352a0da215005
Grant Fritchey <grant@scarydba.com>
2023-01-27 06:29:56 -0600

    Reports output from check runs

--- tests.ps1
+++ tests.ps1
@@ -7,3 +7,15 @@ flyway -dryRunOutput='test.sql' migrate
 flyway -skipCheckForUpdate migrate
 
 flyway -validateOnMigrate=false migrate
+
+
+flyway -skipCheckForUpdate -validateOnMigrate=false migrate
+
+
+flyway -executeInTransaction="false" migrate
+
+
+flyway check -changes "-check.buildUrl=jdbc:sqlserver://localhost;authentication=sqlPassword;databaseName=ADW_Check;encrypt=true;integratedSecurity=false;trustServerCertificate=true" "-check.reportFilename=checkreport.html"
+
+flyway check -code "-check.buildUrl=jdbc:sqlserver://localhost;authentication=sqlPassword;databaseName=ADW_Check;encrypt=true;integratedSecurity=false;trustServerCertificate=true" "-check.reportFilename=codereport.html"
+
